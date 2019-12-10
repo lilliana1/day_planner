9cb9966ae656752bfbdc35075cf3420fb6210f21
Lilliana Ramos <lilliana.cramos@gmail.com>
2019-12-09 21:12:12 -0500

    added buttons and text area

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+//current date displays
 $("#currentDay").text(moment().format('MMMM Do YYYY'));
+
+//
+var saveBtn = document.querySelector(".saveBtn");
+var todoNine = document.querySelector("todo-9");
+var todoTen = document.querySelector("todo-10");
+var todoEleven = document.querySelector("todo-11");
+var todoTwelve = document.querySelector("todo-12");
+var todoOne = document.querySelector("todo-1");
+var todoTwo = document.querySelector("todo-2");
+var todoThree = document.querySelector("todo-3");
+var todoFour = document.querySelector("todo-4");
+var todoFive = document.querySelector("todo-5");
