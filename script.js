6d92fecd3575a3ddfa53d60df06df2429e4ec034
Lilliana Ramos <lilliana.cramos@gmail.com>
2019-12-11 07:58:00 -0500

    commenting what i should do

--- script.js
+++ script.js
@@ -3,14 +3,90 @@ $("#currentDay").text(moment().format('MMMM Do YYYY'));
 
 //
 var saveBtn = document.querySelector(".saveBtn");
-var todoNine = document.querySelector("todo-9");
-var todoTen = document.querySelector("todo-10");
-var todoEleven = document.querySelector("todo-11");
-var todoTwelve = document.querySelector("todo-12");
-var todoOne = document.querySelector("todo-1");
-var todoTwo = document.querySelector("todo-2");
-var todoThree = document.querySelector("todo-3");
-var todoFour = document.querySelector("todo-4");
-var todoFive = document.querySelector("todo-5");
+var nine = document.querySelector("#9");
+var ten = document.querySelector("#10");
+var eleven = document.querySelector("#11");
+var twelve = document.querySelector("#12");
+var one = document.querySelector("#1");
+var two = document.querySelector("#2");
+var three = document.querySelector("#3");
+var four = document.querySelector("#4");
+var five = document.querySelector("#5");
 
-var nine = [];
+
+// Store data when save button is click on 
+$(".saveBtn").on("click", function(){
+    var info = $('#9').val
+    localStorage.setItem("nine",JSON.stringify("9"))
+    // Get data
+    localStorage.getItem('nine');
+})
+
+// Display the items from local storage 
+
+// Change the color of the "Time Block Section" depending of the current time
+
+
+
+
+
+
+
+// $(".saveBtn").on("click", function(){
+    
+//     localStorage.setItem("todoNine", input.val());
+//     // var todoAtNine = JSON.parse(localStorage.setItem("todoNine"))
+//     // todoAtNine.push("todo-9");
+//     // todoAtNine.setItem;
+
+//     var storedValue = localStorage.getItem("todoNine");
+// })
+
+// $('#saveBtn').on('click', function(){
+
+//     $('input[type="text"]').each(function(){    
+//         var id = $(this).attr('id');
+//         var value = $(this).val();
+//        localStorage.setItem(id, value);
+
+//     });   
+// });
+
+// $('#load').on('click', function(){
+//     $('input[type="text"]').each(function(){    
+//         var id = $(this).attr('id');
+//         var value = localStorage.getItem(id);
+
+//         $(this).val(value);
+
+//     }); 
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(function() {
+//     var date = new Date(),
+//       currentDate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+//     $(".justify-content-md-center").each(function() {
+//       var specifiedDate = $(this).data('date');
+//       if (specifiedDate == currentDate) {
+//         $(this).addClass('today');
+//       } else if (currentDate > specifiedDate) {
+//         $(this).addClass('past');
+//       } else {
+//         $(this).addClass('future');
+//       }
+//     });
+//   });
